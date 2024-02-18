004041952e4f413f79abcc2d465600b51a3a5a40
fullstackjava082023 <141508327+fullstackjava082023@users.noreply.github.com>
2024-02-18 19:43:39 +0200

    I am John Snow - i changed the code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello ilia");
+        System.out.println("Hello John");
+        System.out.println("Hello Arja");
     }
 }
